22f5b06fb74f42dcde726c867714cbb70d3fcb2f
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2023-09-18 16:22:59 -0300

    att

--- src/scripts.js
+++ src/scripts.js
@@ -1,4 +1,9 @@
 function jogar(){
-    document.getElementById('jogo').style.display='block'
+    let j = document.getElementById('jogo')
+    j.style.display='block'
+    var select = document.getElementById("ata");
+    var opcaoValor = select.options[select.selectedIndex].value;
+    j.innerHTML += `O seu time é formado por ${opcaoValor}`
+    
 }
 
