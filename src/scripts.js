ad0d173f1c2f4b5359f3c78866b2df3036cbff17
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2023-09-18 18:32:57 -0300

    Pegando os valores do select

--- src/scripts.js
+++ src/scripts.js
@@ -1,9 +1,13 @@
 function jogar(){
     let j = document.getElementById('jogo')
     j.style.display='block'
-    var select = document.getElementById("ata");
-    var opcaoValor = select.options[select.selectedIndex].value;
-    j.innerHTML += `O seu time é formado por ${opcaoValor}`
-    
+    var gol = document.getElementById("gol");
+    var jog1 = gol.options[gol.selectedIndex].text;
+    var zag = document.getElementById("zag");
+    var jog2 = zag.options[zag.selectedIndex].text;
+    var mei = document.getElementById("mei");
+    var jog3 = mei.options[mei.selectedIndex].text;
+    var ata = document.getElementById("ata");
+    var jog4 = ata.options[ata.selectedIndex].text;
+    j.innerHTML += `O seu time é formado por ${jog1}, ${jog2}, ${jog3} e ${jog4}`
 }
-
