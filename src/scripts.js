983ba3dd5e6774558e53ec73886fc8004d8d0f58
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2023-09-18 15:32:54 -0300

    Update scripts.js

--- src/scripts.js
+++ src/scripts.js
@@ -2,11 +2,3 @@ function jogar(){
     document.getElementById('jogo').style.display='block'
 }
 
-function selecionar() {
-    let g = document.getElementById("teste").style.border="2px solid white"
-    }
-
-    function desmarcar() {
-        let g = document.getElementById("teste").style.border="2px solid black" 
-        
-    }   
