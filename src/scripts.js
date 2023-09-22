7ac2c4d4f0620dd31b842221a544cc902995bc5f
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2023-09-22 15:42:44 -0300

    att

--- src/scripts.js
+++ src/scripts.js
@@ -1,10 +1,3 @@
-/* Função para gerar habilidades dos Jogadores */
-function Jogador(nome,passe,chute){
-    this.nome = nome; 
-    this.passe = passe;
-    this.chute = chute;  
-}
-var um = new Jogador("João",60,80)
 /*Função para selecionar os jogadores do time */
 function selecionar(){
     let j = document.getElementById('jogo')
@@ -20,57 +13,18 @@ function selecionar(){
     var ata = document.getElementById("ata");
     var jog5 = ata.options[ata.selectedIndex].text;
     j.innerHTML += `O seu time é formado por ${jog1}, ${jog2}, ${jog3}, ${jog4} e ${jog5}`
+    j.innerHTML += `<div id="teste"><p><input type="button" value="Jogar" onclick="jogar()"></p></div>`
 }
 
-function zoom() {
-    document.querySelectorAll('cartas').style.width = '100px'
-
+function jogar(){
+  var t = document.getElementById('teste')  
+  var g = document.getElementById("gol");
+  var att = g.options[g.selectedIndex];
+  var optionData = JSON.parse(att.getAttribute("data-optionData"));
+  var n = (optionData.nome)
+  var r = (optionData.Reflexo)
+  var s = (optionData.Saida)
+  /* t.innerHTML = "Nome: " + optionData.nome + ", Reflexo: " + optionData.Reflexo + " Saida:" + optionData.Saida;  */
+  t.innerHTMl += `Goleiro:${n} , Reflexo:${r} , Saida:${s}`
 }
-/*
-// Lista de jogadores pré-definidos
-var listaJogadores = ["Messi", "Ronaldo", "Neymar", "Mbappé", "Salah"];
-
-// Função para selecionar os jogadores
-function selecionarTime(numMaxJogadores) {
-  var time = []; // array para armazenar os jogadores selecionados
-  var numJogadores = 0;
-
-  while (numJogadores < numMaxJogadores) {
-    // Exibir a lista de jogadores disponíveis
-    console.log("Lista de jogadores disponíveis:");
-    for (var i = 0; i < listaJogadores.length; i++) {
-      console.log(i + 1 + ". " + listaJogadores[i]);
-    }
-
-    var escolha = parseInt(prompt("Escolha o número do jogador:"));
-
-    // Verificar se o número escolhido é válido
-    if (escolha >= 1 && escolha <= listaJogadores.length) {
-      var jogadorSelecionado = listaJogadores[escolha - 1];
-
-      // Verificar se o jogador já foi selecionado anteriormente
-      if (!time.includes(jogadorSelecionado)) {
-        time.push(jogadorSelecionado); // Adicionar jogador ao time
-        numJogadores++;
-        console.log("Jogador selecionado: " + jogadorSelecionado + "\n");
-      } else {
-        console.log("Esse jogador já foi selecionado. Escolha outro jogador.\n");
-      }
-    } else {
-      console.log("Número inválido. Escolha um número válido.\n");
-    }
-  }
-
-  return time;
-}
-var numMaxJogadores = parseInt(prompt("Digite o número máximo de jogadores no time:"));
-var timeFinal = selecionarTime(numMaxJogadores);
-
-console.log("O time formado é: " + timeFinal.join(", "));
-
-
-Neste exemplo, a lista de jogadores pré-definidos é armazenada na variável `listaJogadores`. A função `selecionarTime` é responsável por permitir ao usuário selecionar jogadores dessa lista. O usuário pode escolher o número do jogador e esse número é verificado para garantir que é válido. Se o número for válido e o jogador ainda não foi selecionado, ele é adicionado ao array `time`. Caso contrário, uma mensagem de erro é exibida. O processo continua até que o número de jogadores selecionados atinja o número máximo fornecido pelo usuário. Finalmente, o time formado é exibido na tela.
-sex, 3:58 PM
-
-*/
 
